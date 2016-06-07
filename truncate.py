117e171390261c03c22a250185ebbdcebbf8fcc4
hw20686832 <stef-hw@163.com>
2016-06-07 16:47:27 +0800

    updated

---
+++ truncate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding:utf-8
+import sys
+
+import MySQLdb
+
+
+db = MySQLdb.connect(host='192.168.2.20', user='moviebox',
+                     passwd='moviebox', db='moviebox')
+
+
+def truncate(*tables):
+    cursor = db.cursor()
+    for table in tables:
+        sql = "delete from %s" % table
+        cursor.execute(sql)
+    db.commit()
+
+
+if __name__ == '__main__':
+    try:
+        tables = sys.argv[1:]
+    except:
+        tables = ['category', 'category_trans', 'movie',
+                  'recommend', 'trailer', 'trailer_source',
+                  'tv', 'tv_episode', 'tv_season']
+
+    truncate(*tables)
